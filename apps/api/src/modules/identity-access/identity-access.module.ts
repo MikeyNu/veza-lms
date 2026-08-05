2e26e2e0c5b7a11290406c60ee8a9843e4f90926
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 07:12:02 +0200

    feat: register person identity acceptance bridge

--- apps/api/src/modules/identity-access/identity-access.module.ts
+++ apps/api/src/modules/identity-access/identity-access.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AccessAdministrationService } from "./application/access-administration.service.js";
 import { AccessDirectoryQueryService } from "./application/access-directory-query.service.js";
 import { MembershipInvitationService } from "./application/membership-invitation.service.js";
+import { PersonIdentityAcceptanceService } from "./application/person-identity-acceptance.service.js";
 import { AccessDirectoryController } from "./http/access-directory.controller.js";
 import { MembershipInvitationsController } from "./http/membership-invitations.controller.js";
 import { PrincipalSessionController } from "./http/principal-session.controller.js";
@@ -21,6 +22,7 @@ import { InvitationTokenService } from "./security/invitation-token.service.js";
     InvitationTokenService,
     AccessDirectoryQueryService,
     AccessAdministrationService,
+    PersonIdentityAcceptanceService,
     MembershipInvitationService,
   ],
   exports: [
