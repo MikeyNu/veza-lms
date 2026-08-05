b63b623c3e8490552158fecf3043065b6c00c8cd
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 06:08:51 +0200

    feat: register institution relationship service

--- apps/api/src/modules/people/people.module.ts
+++ apps/api/src/modules/people/people.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { InstitutionRelationshipService } from "./application/institution-relationship.service.js";
 import { PeopleIntegrityService } from "./application/people-integrity.service.js";
 import { PeopleQueryService } from "./application/people-query.service.js";
 import { PeopleService } from "./application/people.service.js";
@@ -6,7 +7,17 @@ import { PeopleController } from "./http/people.controller.js";
 
 @Module({
   controllers: [PeopleController],
-  providers: [PeopleService, PeopleIntegrityService, PeopleQueryService],
-  exports: [PeopleService, PeopleIntegrityService, PeopleQueryService],
+  providers: [
+    PeopleService,
+    PeopleIntegrityService,
+    PeopleQueryService,
+    InstitutionRelationshipService,
+  ],
+  exports: [
+    PeopleService,
+    PeopleIntegrityService,
+    PeopleQueryService,
+    InstitutionRelationshipService,
+  ],
 })
 export class PeopleModule {}
