27727869c267a1cd912e63814975fd545ff942f8
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 06:33:00 +0200

    feat: register catalogue governance controller

--- apps/api/src/modules/catalogue/catalogue.module.ts
+++ apps/api/src/modules/catalogue/catalogue.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
+import { CatalogueGovernanceService } from "./application/catalogue-governance.service.js";
 import { CatalogueReferenceService } from "./application/catalogue-reference.service.js";
 import { CatalogueService } from "./application/catalogue.service.js";
+import { CatalogueGovernanceController } from "./http/catalogue-governance.controller.js";
 import { CatalogueController } from "./http/catalogue.controller.js";
 
 @Module({
-  controllers: [CatalogueController],
-  providers: [CatalogueService, CatalogueReferenceService],
-  exports: [CatalogueService, CatalogueReferenceService],
+  controllers: [CatalogueController, CatalogueGovernanceController],
+  providers: [CatalogueService, CatalogueReferenceService, CatalogueGovernanceService],
+  exports: [CatalogueService, CatalogueReferenceService, CatalogueGovernanceService],
 })
 export class CatalogueModule {}
