d515afa0343aa2ba6cf4d6b5c7415a4ef26bd6d1
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 05:48:30 +0200

    feat: register people integrity workflows

--- apps/api/src/modules/people/people.module.ts
+++ apps/api/src/modules/people/people.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PeopleIntegrityService } from "./application/people-integrity.service.js";
 import { PeopleService } from "./application/people.service.js";
 import { PeopleController } from "./http/people.controller.js";
 
 @Module({
   controllers: [PeopleController],
-  providers: [PeopleService],
-  exports: [PeopleService],
+  providers: [PeopleService, PeopleIntegrityService],
+  exports: [PeopleService, PeopleIntegrityService],
 })
 export class PeopleModule {}
