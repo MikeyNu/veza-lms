c4c658afc28cc9edba3508f9bebe229c68a74e7a
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 06:26:41 +0200

    feat: register catalogue reference queries

--- apps/api/src/modules/catalogue/catalogue.module.ts
+++ apps/api/src/modules/catalogue/catalogue.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { CatalogueReferenceService } from "./application/catalogue-reference.service.js";
 import { CatalogueService } from "./application/catalogue.service.js";
 import { CatalogueController } from "./http/catalogue.controller.js";
 
 @Module({
   controllers: [CatalogueController],
-  providers: [CatalogueService],
-  exports: [CatalogueService],
+  providers: [CatalogueService, CatalogueReferenceService],
+  exports: [CatalogueService, CatalogueReferenceService],
 })
 export class CatalogueModule {}
