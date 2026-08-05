64e0fcc05039aaa64e66f8c1479df410ad721e11
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 07:10:31 +0200

    feat: register complete people operations

--- apps/api/src/modules/people/people.module.ts
+++ apps/api/src/modules/people/people.module.ts
@@ -1,22 +1,33 @@
 import { Module } from "@nestjs/common";
+import { IdentityAccessModule } from "../identity-access/identity-access.module.js";
 import { InstitutionRelationshipService } from "./application/institution-relationship.service.js";
+import { PeopleIdentityLinkService } from "./application/people-identity-link.service.js";
+import { PeopleInstitutionBoundaryService } from "./application/people-institution-boundary.service.js";
 import { PeopleIntegrityService } from "./application/people-integrity.service.js";
+import { PeopleOperationsService } from "./application/people-operations.service.js";
 import { PeopleQueryService } from "./application/people-query.service.js";
 import { PeopleService } from "./application/people.service.js";
+import { PeopleOperationsController } from "./http/people-operations.controller.js";
 import { PeopleController } from "./http/people.controller.js";
 
 @Module({
-  controllers: [PeopleController],
+  imports: [IdentityAccessModule],
+  controllers: [PeopleController, PeopleOperationsController],
   providers: [
     PeopleService,
     PeopleIntegrityService,
     PeopleQueryService,
+    PeopleOperationsService,
+    PeopleIdentityLinkService,
+    PeopleInstitutionBoundaryService,
     InstitutionRelationshipService,
   ],
   exports: [
     PeopleService,
     PeopleIntegrityService,
     PeopleQueryService,
+    PeopleOperationsService,
+    PeopleIdentityLinkService,
     InstitutionRelationshipService,
   ],
 })
