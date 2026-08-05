d0135b13db6ed5022d2c5c58ca6834463a9d6954
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-05 16:02:16 +0200

    feat(storage): register administration services

--- apps/api/src/platform/storage/storage.module.ts
+++ apps/api/src/platform/storage/storage.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from "@nestjs/common";
 import { S3CompatibleSigner } from "./s3-compatible-signer.js";
+import { StorageAdministrationService } from "./storage-administration.service.js";
 import { StorageController } from "./storage.controller.js";
 import { StorageService } from "./storage.service.js";
 
 @Global()
 @Module({
   controllers: [StorageController],
-  providers: [S3CompatibleSigner, StorageService],
-  exports: [S3CompatibleSigner, StorageService],
+  providers: [S3CompatibleSigner, StorageAdministrationService, StorageService],
+  exports: [S3CompatibleSigner, StorageAdministrationService, StorageService],
 })
 export class StorageModule {}
