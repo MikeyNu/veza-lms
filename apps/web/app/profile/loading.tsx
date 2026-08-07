553e4c1632886f42c439c6d00695313147c27b9f
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-07 19:30:12 +0200

    Add profile loading state

---
+++ apps/web/app/profile/loading.tsx
@@ -0,0 +1,5 @@
+import { WorkspaceRouteLoading } from "../../src/components/states/workspace-route-loading";
+
+export default function ProfileLoading() {
+  return <WorkspaceRouteLoading eyebrow="ACCOUNT" title="Loading your profile" />;
+}
