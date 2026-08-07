1f1bb8c70dd0676b12afa4a59e14158c6d5f6d88
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-07 19:43:14 +0200

    Register communications recipient workspace

--- apps/api/src/platform/communications/communications.module.ts
+++ apps/api/src/platform/communications/communications.module.ts
@@ -1,17 +1,20 @@
 import { Global, Module } from "@nestjs/common";
 import { CommunicationsControlController } from "./communications-control.controller.js";
 import { CommunicationsProviderController } from "./communications-provider.controller.js";
+import { CommunicationsRecipientController } from "./communications-recipient.controller.js";
 import { CommunicationsController } from "./communications.controller.js";
+import { CommunicationsRecipientService } from "./communications-recipient.service.js";
 import { CommunicationsService } from "./communications.service.js";
 
 @Global()
 @Module({
   controllers: [
     CommunicationsController,
+    CommunicationsRecipientController,
     CommunicationsProviderController,
     CommunicationsControlController,
   ],
-  providers: [CommunicationsService],
-  exports: [CommunicationsService],
+  providers: [CommunicationsService, CommunicationsRecipientService],
+  exports: [CommunicationsService, CommunicationsRecipientService],
 })
 export class CommunicationsModule {}
